698308eec249829a8e56bae5d8b05480a0e47748
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-24 16:17:29 +0200

    added in markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form>
+          <label>Name</label>
+          <input type="text" name="name" required />
+          <button type="text">Add contact</button>
+        </form>
+        <ul>
+          <li>Rosie Simpson</li>
+          <li>Hermonie Cline</li>
+          <li>Eden Clements</li>
+        </ul>
+      </div>
+    );
+  }
+}
