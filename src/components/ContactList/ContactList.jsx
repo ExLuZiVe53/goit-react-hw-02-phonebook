13a032802670b4ef577b9ac2a867bfba363eb91a
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-25 19:34:33 +0200

    added ContactList

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const ContactList = ({ contacts, onRemoveContact }) => (
+  <ul className="ul">
+    {contacts.map(contact => (
+      <li className="li" key={contact.id}>
+        {contact.name + ':' + contact.number}
+        {
+          <button
+            className="delete"
+            type="button"
+            name="delete"
+            onClick={() => onRemoveContact(contact.id)}
+          >
+            delete
+          </button>
+        }
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  onRemoveContact: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default ContactList;
